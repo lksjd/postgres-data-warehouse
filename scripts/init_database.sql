4e16bcf43b92f9887ce6f5afd7bd3134a4b2fedd
Zapryan <112425709+lksjd@users.noreply.github.com>
2026-03-01 11:47:43 +0200

    Update init_database.sql

--- scripts/init_database.sql
+++ scripts/init_database.sql
@@ -1,16 +1,29 @@
 /*
 =============================================================
-Create Schemas for DataWarehouse
+Create Database and Schemas
 =============================================================
 Script Purpose:
-    This script creates three schemas within the connected PostgreSQL database:
-    'bronze', 'silver', and 'gold'.
-    
+    This script creates a new database named 'data_warehouse' after checking if it already exists. 
+    If the database exists, it is dropped and recreated. Additionally, the script sets up three schemas 
+    within the database: 'bronze', 'silver', and 'gold'.
+	
 WARNING:
-    Running this script multiple times is safe because it uses IF NOT EXISTS.
+    Running this script will drop the entire 'data_warehouse' database if it exists. 
+    All data in the database will be permanently deleted. Proceed with caution 
+    and ensure you have proper backups before running this script.
 */
 
--- Create Schemas
+-- Drop database if it exists
+DROP DATABASE IF EXISTS "data_warehouse";
+
+-- Create database
+CREATE DATABASE "data_warehouse";
+
+-- Connect to the new database
+
+-- Create schemas
 CREATE SCHEMA IF NOT EXISTS bronze;
+
 CREATE SCHEMA IF NOT EXISTS silver;
+
 CREATE SCHEMA IF NOT EXISTS gold;
